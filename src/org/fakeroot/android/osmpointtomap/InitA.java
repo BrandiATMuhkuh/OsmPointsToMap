41edc2dcf0d2d456e362041b0255446e5ba09aac
Jürgen Brandstetter <juergen@tagaprice.org>
2011-05-16 19:55:52 +0200

    It is now possible to start the initA.class without an intent

--- src/org/fakeroot/android/osmpointtomap/InitA.java
+++ src/org/fakeroot/android/osmpointtomap/InitA.java
@@ -38,7 +38,7 @@ public class InitA extends TabActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.init);
 
 		Resources res = getResources(); // Resource object to get Drawables
 		TabHost tabHost = getTabHost(); // The activity TabHost
